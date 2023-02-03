4b4a123261d074c9f9a7f1cffd0ed6a841726377
davidr99 <davidr99@gmail.com>
2023-02-02 20:23:37 -0500

    Update keyboard decoder
    Changes to make the keyboard decoder work at all CPU speeds

--- KeyboardDecoder.v
+++ KeyboardDecoder.v
@@ -9,48 +9,53 @@ module KeyboardDecoder(Clock, Keyb_Row_I, Keyb_Col_O, Keyb_Value);
 	
 	// Maped keyboard
 	output reg [5:0] Keyb_Value;
+	
 	reg [5:0] Value;
+	reg [2:0] CurrentCol;
+	reg [3:0] CurrentColVal;
 	
-	reg [2:0] CycleCount;
-	reg [1:0] CurrentRow;
-	reg [3:0] CurrentRowCount;
+	reg [1:0] cycle;
 	
 	
-	assign Keyb_Col_O = 1 << CurrentRow;
+	assign Keyb_Col_O = 1 << CurrentCol;
 
 	
 	always @ (posedge Clock)
-		begin				
-			// This will hold the value on the output for 8 Clock cycles
-			if (CycleCount == 4)
-				begin
-					Keyb_Value = Value;
+		begin
+			cycle = cycle + 1;		
+			if (cycle == 2'b11)
+				begin			
+				
+					if (CurrentCol == 5)
+						begin
+							Value <= 0;
+							CurrentCol <= 0;
+							CurrentColVal <= 0;
+							Keyb_Value <= Value;
+						end
+					else
+						begin
+							CurrentCol <= CurrentCol + 1;
+							CurrentColVal <= (CurrentCol + 1) << 2;
+						end
+						
+					if (Keyb_Row_I[0] && CurrentCol < 5)
+						begin
+							Value <= CurrentColVal + 1;
+						end				
+					else if (Keyb_Row_I[1] && CurrentCol < 5)
+						begin
+							Value <= CurrentColVal + 2;
+						end				
+					else if (Keyb_Row_I[2] && CurrentCol < 5)
+						begin
+							Value <= CurrentColVal + 3;
+						end				
+					else if (Keyb_Row_I[3] && CurrentCol < 5)
+						begin
+							Value <= CurrentColVal + 4;
+						end
 				end
-		
-			if (Keyb_Row_I[0])
-				begin
-					Value = CurrentRowCount + 1;
-				end				
-			else if (Keyb_Row_I[1])
-				begin
-					Value = CurrentRowCount + 2;
-				end				
-			else if (Keyb_Row_I[2])
-				begin
-					Value = CurrentRowCount + 3;
-				end				
-			else if (Keyb_Row_I[3])
-				begin
-					Value = CurrentRowCount + 4;
-				end
-			else if (CurrentRow == 0)
-				begin
-					Value = 0;
-				end
-							
-			CurrentRowCount = CurrentRow * 4;
-			CycleCount = CycleCount + 1;
-			CurrentRow = CurrentRow + 1;
 		end
 			
 			
